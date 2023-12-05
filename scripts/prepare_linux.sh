290abc964c18b488b82f67c2afaa3e09a876c1d9
Alex <AlexZhu2001@stu.xupt.edu.cn>
2023-12-05 16:31:42 +0800

    Fix buid action linux and deploy tar

--- scripts/prepare_linux.sh
+++ scripts/prepare_linux.sh
@@ -3,6 +3,5 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-chmod +x ./scripts/install_cuda_ubuntu.sh
 bash ./scripts/install_cuda_ubuntu.sh
 sudo apt-get install -y gcc-11 g++-11
