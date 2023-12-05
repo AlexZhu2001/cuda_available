be4cad6f703f274344e7f9ee3fd1afae2011b454
Alex <AlexZhu2001@stu.xupt.edu.cn>
2023-12-05 15:40:20 +0800

    Fix linux build, deploy tar

---
+++ scripts/prepare_linux.sh
@@ -0,0 +1,8 @@
+# Copyright (c) 2023 Alex
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+chmod +x ./scripts/install_cuda_ubuntu.sh
+bash ./scripts/install_cuda_ubuntu.sh
+sudo apt-get install -y gcc-11 g++-11
