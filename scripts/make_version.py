7921e849300c1a008635bd2f62d9ecab50686498
Alex <AlexZhu2001@stu.xupt.edu.cn>
2023-12-07 11:48:22 +0800

    Fix no metadata

---
+++ scripts/make_version.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2023 Alex
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+import os
+import sys
+
+VERSION = "0.1.0"
+
+if __name__ == "__main__":
+    cuda_ver = os.environ.get("CUDA_VERSION")
+    if cuda_ver is None:
+        print("CUDA_VERSION is not set!")
+        sys.exit(-1)
+    major,minor = cuda_ver.split(".")[:2]
+    fp = "./src/VERSION.metadata"
+    with open(fp, "w") as f:
+        text = f"VERSION={VERSION}+cu{major}{minor}"
+        f.write(text)
+        print(f"Result {text} write to {fp} succeed")
+    
